c85eba1f9ad245f754d04401141b986f1fc86874
tekimbaev <abai.tekimbaev@artware.dev>
2022-12-06 14:04:48 +0600

    created second map

--- bin/lesson_2.dart
+++ bin/lesson_2.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 void main() {
   Map noteBook = {
     'Abai': 123123123123,
@@ -9,6 +7,13 @@ void main() {
     'Ab123ai': 0554828888,
   };
 
+  Map noteBook2 = {
+    'Abai': 123123123123,
+    'Arsen': 0554828888,
+    'Argen': 0554828888,
+    'A123bai': 0554828888,
+    'Ab123ai': 0554828888,
+  };
   noteBook.addAll({
     'Abai333': 123123123123,
   });
