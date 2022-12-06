05837a97d9ea6839416c11d61161ada539ac35aa
tekimbaev <abai.tekimbaev@artware.dev>
2022-12-06 14:01:53 +0600

    created notebook

---
+++ bin/lesson_2.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+void main() {
+  Map noteBook = {
+    'Abai': 123123123123,
+    'Arsen': 0554828888,
+    'Argen': 0554828888,
+    'A123bai': 0554828888,
+    'Ab123ai': 0554828888,
+  };
+
+  noteBook.addAll({
+    'Abai333': 123123123123,
+  });
+
+  print(noteBook.containsKey('Argen'));
+  print(noteBook.containsValue(055482888823));
+  noteBook.remove('Arsen');
+
+  print(noteBook);
+}
